5598c00ad41440e8e075be2d0169d619912dc5be
Zorex Salvo <zorexsalvo@gmail.com>
2019-10-01 19:05:22 +0800

    Create model for product category

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 
 admin.site.register(Product)
+admin.site.register(Category)
 admin.site.site_header = 'Inventory System'
--- product/models.py
+++ product/models.py
@@ -5,4 +5,21 @@ from model_utils.models import TimeStampedModel
 
 class Product(TimeStampedModel):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    category = models.ForeignKey('Category',
+                                 on_delete=models.CASCADE,
+                                 null=True)
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(TimeStampedModel):
+    code = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return '{} - {}'.format(self.code, self.name)
